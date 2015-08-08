b0480e4b2a9450f9f86664900e43e78b3328661d
elifcan <can.elif.95@gmail.com>
2015-08-08 22:34:15 +0300

    1. gün yapılanlar

---
+++ asd/deneme1.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8"/>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 08.08.2015
+ * Time: 18:33
+ */
+$sayi1 = 5;
+$sayi2 = 3;
+
+//echo $sayi1 - $sayi2;
+//echo $sayi1 + $sayi2;
+//echo $sayi1 * $sayi2;
+//echo $sayi1 / $sayi2;
+//echo $sayi1 % $sayi2;
+//echo ceil($sayi1 / $sayi2); //Aşağı yuvarlama
+$sayi1++;
+echo $sayi1;
+echo ++$sayi2;
+//echo $$sayi2; //sayi2den önceki değişkeni atıyor.
+
+
+?>
